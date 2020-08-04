3a9b2972e4363aedc405878f06ee17ac85f184ec
ynding <913690560@qq.com>
2020-08-04 17:23:58 +0800

    版本由父pom统一管理

--- src/main/java/com/ynding/cloud/physical/dataway/meta/PhysicalDatawayMetaApplication.java
+++ src/main/java/com/ynding/cloud/physical/dataway/meta/PhysicalDatawayMetaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableHasor() // 在Spring 中启用 Hasor
-@EnableHasorWeb()  // 将 hasor-web 配置到 Spring 环境中，Dataway 的 UI 是通过 hasor-web 提供服务。
+@EnableHasor // 在Spring 中启用 Hasor
+@EnableHasorWeb  // 将 hasor-web 配置到 Spring 环境中，Dataway 的 UI 是通过 hasor-web 提供服务。
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PhysicalDatawayMetaApplication {
